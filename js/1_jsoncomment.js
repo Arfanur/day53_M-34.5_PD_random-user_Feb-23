13679458b0a4bbbc45ee108385e4946ad4c18b96
Arfanur <arfanur03@gmail.com>
2022-06-20 11:24:20 +0600

    API checkList 4 completed (using Arrow Function & forEach)

--- js/1_jsoncomment.js
+++ js/1_jsoncomment.js
@@ -8,8 +8,9 @@ const loadComments = async () => {
     const data = await res.json();
     displayComments(data);
 };
+// loadComments();
 
-const displayComments = (comments) => {
+const displayComments = comments => {
     const commentsDiv = document.getElementById('comments');
     // for(const comment of comments){
     //     console.log(comment);
@@ -19,7 +20,7 @@ const displayComments = (comments) => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="border d-inline-block my-1 py-2 rounded">
-            <h4><span class="text-success">Id:</span> ${comment.id}</h4>
+            <h4><span class="text-success">Id:</span> <span class="text-warning">${comment.id}</span></h4>
             <h5><span class="text-success">Name:</span> ${comment.name}</h5>
             <h5><span class="text-success">Email:</span> ${comment.email}</h5>
             <h6><span class="text-success">Body:</span> ${comment.body}</h6>
